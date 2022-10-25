af4dc858bf9b2d7ce3c15f42f0c520b4bfa1d980
xychen <xychen@listenai.com>
2022-10-25 15:42:52 +0800

    Format code

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,7 @@
   <div :style="{ padding: '16px' }">
     <a-row type="flex" :gutter="16">
       <a-col>
-        <a-button
-          icon="usb"
-          @click="open"
-          :loading="busy"
-          v-if="!opened"
-          type="primary"
-        >
+        <a-button icon="usb" @click="open" :loading="busy" v-if="!opened" type="primary">
           打开设备
         </a-button>
         <a-button icon="disconnect" @click="close" :loading="busy" v-else>
@@ -17,12 +11,7 @@
       </a-col>
 
       <a-col>
-        <a-button
-          icon="download"
-          @click="startRecord"
-          :disabled="!opened"
-          v-if="!recording"
-        >
+        <a-button icon="download" @click="startRecord" :disabled="!opened" v-if="!recording">
           保存音频
         </a-button>
         <a-button type="danger" @click="stopRecord" v-else>
@@ -31,29 +20,17 @@
       </a-col>
 
       <a-col>
-        <a-select
-          v-model="sampleRate"
-          :disabled="opened"
-          :style="{ width: '100px' }"
-        >
+        <a-select v-model="sampleRate" :disabled="opened" :style="{ width: '100px' }">
           <a-select-option :value="16000">16 kHz</a-select-option>
         </a-select>
       </a-col>
       <a-col>
-        <a-select
-          v-model="bitDepth"
-          :disabled="opened"
-          :style="{ width: '100px' }"
-        >
+        <a-select v-model="bitDepth" :disabled="opened" :style="{ width: '100px' }">
           <a-select-option :value="32">32 bit</a-select-option>
         </a-select>
       </a-col>
       <a-col>
-        <a-select
-          v-model="channels"
-          :disabled="opened"
-          :style="{ width: '100px' }"
-        >
+        <a-select v-model="channels" :disabled="opened" :style="{ width: '100px' }">
           <a-select-option :value="1">1 ch</a-select-option>
           <a-select-option :value="2">2 ch</a-select-option>
           <a-select-option :value="3">3 ch</a-select-option>
@@ -64,30 +41,14 @@
       </a-col>
     </a-row>
     <div :style="{ width: '300px', marginTop: '32px' }">
-      <a-row
-        type="flex"
-        v-for="(track, index) of tracks"
-        :key="index"
-        :gutter="16"
-        :style="{ marginTop: '8px' }"
-      >
+      <a-row type="flex" v-for="(track, index) of tracks" :key="index" :gutter="16" :style="{ marginTop: '8px' }">
         <a-col>
-          <a-button
-            size="small"
-            icon="sound"
-            shape="circle"
-            :type="track.muted ? 'default' : 'primary'"
-            @click="() => toggleTrackMuted(index)"
-          />
+          <a-button size="small" icon="sound" shape="circle" :type="track.muted ? 'default' : 'primary'"
+            @click="() => toggleTrackMuted(index)" />
         </a-col>
         <a-col flex="auto">
-          <a-progress
-            size="small"
-            status="normal"
-            :show-info="false"
-            :percent="peaks[index]"
-            :stroke-color="track.muted ? 'lightgrey' : undefined"
-          />
+          <a-progress size="small" status="normal" :show-info="false" :percent="peaks[index]"
+            :stroke-color="track.muted ? 'lightgrey' : undefined" />
         </a-col>
       </a-row>
     </div>
